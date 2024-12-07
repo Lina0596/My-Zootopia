91da3e82adee8fd4805a0f8db44cc5b40669a36d
Lina0596 <lina.dahlhaus@icloud.com>
2024-12-07 17:02:15 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,17 +2,43 @@ import json
 
 def load_data(file_path):
     """ Loads a JSON file """
-    with open (file_path, "r") as handle:
+    with open(file_path, "r") as handle:
         return json.load(handle)
 
 animals_data = load_data("animals_data.json")
 
-for animal in animals_data:
-    name = animal["name"]
-    diet = animal["characteristics"]["diet"]
-    location = animal["locations"][0]
-    if "type" in animal["characteristics"]:
-        type = animal["characteristics"]["type"]
-        print(f"Name: {name}\nDiet: {diet}\nLocation: {location}\nType: {type}\n")
-    else:
-        print(f"Name: {name}\nDiet: {diet}\nLocation: {location}\n")
+
+def get_data(animals_data):
+    """ Generates content from JSON file """
+    output = ""
+    for animal in animals_data:
+        if "name" in animal:
+            name = animal["name"]
+            output += f"Name: {name}\n"
+        if "diet" in animal["characteristics"]:
+            diet = animal["characteristics"]["diet"]
+            output += f"Diet: {diet}\n"
+        if "locations" in animal:
+            location = animal["locations"][0]
+            output += f"Location: {location}\n"
+        if "type" in animal["characteristics"]:
+            type = animal["characteristics"]["type"]
+            output += f"Type: {type}\n"
+    return output
+
+data_str = get_data(animals_data)
+
+
+def content_html(html_file_path, data_str, new_html_file_path):
+    """ Places content in html file and generates a new html file """
+    with open(html_file_path, "r") as html_file:
+        html_data = html_file.read()
+        new_html_data = html_data.replace("__REPLACE_ANIMALS_INFO__", data_str)
+    with open(new_html_file_path, "w") as html_file:
+        html_file.write(new_html_data)
+
+content_html("animals_template.html", data_str, "animals.html")
+
+
+
+
